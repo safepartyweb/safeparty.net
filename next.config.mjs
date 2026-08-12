707e620fd872f3e3d23435c1109bcdb1c23cf088
coder-mahmud <mahmud.linked@gmail.com>
2026-08-13 02:41:19 +0600

    Simpler layout test3

--- next.config.mjs
+++ next.config.mjs
@@ -13,6 +13,6 @@ const nextConfig = {
 export default nextConfig
 
 // ✅ Add this to enable middleware on all pages except static files and API routes
-export const config = {
-  matcher: ['/((?!api|_next|.*\\..*).*)'], // exclude API, _next, and static assets
-}
+// export const config = {
+//   matcher: ['/((?!api|_next|.*\\..*).*)'], // exclude API, _next, and static assets
+// }
